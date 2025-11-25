cb486a201e25a678d645cf8fd87add453e19d8c4
/ <kojiro.yamamoto@finatext.com>
2025-11-25 09:27:53 +0900

    add models

---
+++ script/20251124_dbtのバージョン管理/jaffle-shop/models/marts/dim_customers_v1.sql
@@ -0,0 +1,17 @@
+with src as (
+select 
+    customer_id,
+    customer_name,
+    floor(random() * 60) + 20 as age
+from {{ ref('stg_customers') }}
+)
+select 
+    customer_id,
+    customer_name,
+    age,
+    case
+        when age < 30 then 'Young Adult'
+        when age between 30 and 50 then 'Adult'
+        else 'Senior'
+    end as age_group
+from src
